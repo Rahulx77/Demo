b1216d3a71380025b587c64f9a1e4ba6e2f33f24
Rahulx77 <bayannomusical@gmail.com>
2025-01-26 19:51:04 +0545

    new for loop file

---
+++ for loop.py
@@ -0,0 +1,26 @@
+"""Write a python program to reverse a given list using for loop"""
+"""a=[1,2,3,4]
+for i in range(len(a)-1,-1,-1):
+    print(a[i],end='',)"""
+
+"""Write a python program to reverse a string using forloop."""
+"""a='python'
+for i in range(len(a)-1,-1,-1):
+    print(a[i],end='')"""
+
+"""Write a python program to print a multiplication table of any number using for loop"""
+"""a=int(input('enter a number= '))
+b=1
+for i in range(1,11):
+    print(a,'*',b,'=',a*i)
+    b+=1"""
+
+"""Write a program to print each item in a list of fruits: ["apple", "banana", "cherry"]"""
+"""f=['apple','banana','cherry']
+for i in f:
+    print(i)"""
+
+"""Write a program to print the following using forloop a. first 10 even numbers"""
+"""for i in range(1,11):
+    if i%2==0:
+        print(i)"""
